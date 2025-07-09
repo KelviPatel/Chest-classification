8fab6232e69745b9e6be1613115008559319b21a
kelvi <patelkelvi23@gmail.com>
2025-07-09 18:28:00 +0530

    data ingestiopn 1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from cnnclassifer import logger
-logger.info("hello world")
+from cnnclassifer.pipeline.data_ingestion_01  import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} started")
+    obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
