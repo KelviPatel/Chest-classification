015f1c61cfc5a165034b4fd15ae2801889100489
kelvi <patelkelvi23@gmail.com>
2025-07-15 17:53:28 +0530

    testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 from cnnclassifer.utils.common import decodeImage
 from cnnclassifer.pipeline.prediction import PredictionPipeline
 
-
+#environment
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
